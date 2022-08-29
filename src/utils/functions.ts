0b686225f0e61e981b2b0c356234184bd0aba8fa
VaibhaV <vaibhakumaryou2@gmail.com>
2022-08-29 14:50:34 +0530

    test

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,36 +1,57 @@
 export const addInArray = (array:any,path:any,elemForAdd:any) =>{
 
-    const oneNested = (array:any,path:any,elemForAdd:any) =>{
-            if (path.slice(1)[0] === undefined) {
-                array[path[0]].files.push(elemForAdd)
-            }else{
-                oneNested(array[path[0]].files,path.slice(1),elemForAdd)
-            }
-    }
-        // If Its Starting Folder
-        if (path[0] === undefined) {
-            array.push(elemForAdd)
-        // If Its in Nested Folder
-        }else{
-            oneNested(array,path,elemForAdd)
-        }
+    const oneNested:any = (array:any,path:any,elemForAdd:any) => (path.slice(1)[0] === undefined) ? array[path[0]].files.push(elemForAdd) : oneNested(array[path[0]].files,path.slice(1),elemForAdd)
+
+    // If Its Starting Folder else Nested Folder
+    path[0] === undefined ? array.push(elemForAdd) : oneNested(array,path,elemForAdd)
+
     return array
 }
+
 export const removeInArray = (array:any,path:any,index:number) =>{
 
-    const oneNested = (array:any,path:any,index:any) =>{
-            if (path.slice(1)[0] === undefined) {
-                array[path[0]].files.splice(index, 1);
-            }else{
-                oneNested(array[path[0]].files,path.slice(1),index)
-            }
-    }
-        // If Its Starting Folder
-        if (path[0] === undefined) {
-            array.splice(index, 1);
-        // If Its in Nested Folder
-        }else{
-            oneNested(array,path,index)
-        }
+    const oneNested:any = (array:any,path:any,index:any) =>path.slice(1)[0] === undefined ? array[path[0]].files.splice(index, 1) : oneNested(array[path[0]].files,path.slice(1),index)
+        
+    // If Its Starting Folder else Nested Folder
+    path[0] === undefined ? array.splice(index, 1) :  oneNested(array,path,index)
+
     return array
 }
+
+
+// export const addInArray = (array:any,path:any,elemForAdd:any) =>{
+
+//     const oneNested = (array:any,path:any,elemForAdd:any) =>{
+//             if (path.slice(1)[0] === undefined) {
+//                 array[path[0]].files.push(elemForAdd)
+//             }else{
+//                 oneNested(array[path[0]].files,path.slice(1),elemForAdd)
+//             }
+//     }
+//         // If Its Starting Folder
+//         if (path[0] === undefined) {
+//             array.push(elemForAdd)
+//         // If Its in Nested Folder
+//         }else{
+//             oneNested(array,path,elemForAdd)
+//         }
+//     return array
+// }
+// export const removeInArray = (array:any,path:any,index:number) =>{
+
+//     const oneNested = (array:any,path:any,index:any) =>{
+//             if (path.slice(1)[0] === undefined) {
+//                 array[path[0]].files.splice(index, 1);
+//             }else{
+//                 oneNested(array[path[0]].files,path.slice(1),index)
+//             }
+//     }
+//         // If Its Starting Folder
+//         if (path[0] === undefined) {
+//             array.splice(index, 1);
+//         // If Its in Nested Folder
+//         }else{
+//             oneNested(array,path,index)
+//         }
+//     return array
+// }
